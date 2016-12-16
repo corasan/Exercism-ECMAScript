9f5f094bfacff734ca2c5dafedfe1912f673b3d5
Henry Paulino <henrypl360@gmail.com>
2016-12-16 15:34:18 -0500

    better solution

--- leap/babel-output/leap.js
+++ leap/babel-output/leap.js
@@ -4,15 +4,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 var isLeapYear = function isLeapYear(year) {
-  if (year % 4 !== 0) {
-    return false;
-  } else if (year % 100 !== 0) {
-    return true;
-  } else if (year % 400 !== 0) {
-    return false;
-  } else {
-    return true;
-  }
+  return year = year % 4 == 0 && !(year % 100 == 0) || year % 400 == 0;
 };
 
 exports["default"] = isLeapYear;
--- leap/leap.js
+++ leap/leap.js
@@ -1,13 +1,5 @@
 const isLeapYear = (year) => {
-  if (year % 4 !== 0) {
-    return false;
-  } else if (year % 100 !== 0) {
-    return true;
-  } else if (year % 400 !== 0) {
-    return false;
-  } else {
-    return true;
-  }
+  return year = (year % 4 == 0) && !(year % 100 == 0) || (year % 400 == 0);
 }
 
 export default isLeapYear;
